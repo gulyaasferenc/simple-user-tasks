219be25508193a5955e0b4b82a30f68760bc5a04
gulyaasferenc <gulyaasferenc@gmail.com>
2023-11-11 00:01:36 +0100

    Remove dist

--- dist/routes/users.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const users_1 = require("../controller/users");
-const router = (0, express_1.Router)();
-router.post('/', users_1.createUser);
-exports.default = router;
